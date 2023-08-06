f30b560edcae2799b926d397552a283da539b100
Gabriel Gomes <bielgomesluz@live.com>
2023-08-05 22:51:28 -0300

    Criação AccountController para login

---
+++ Locals/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Locals.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locals.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel(){
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            //atribuindo a user a busca pelo user no banco por meio do identity
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+
+            //caso exista
+            if (user != null)
+            {
+                //verificação senha, cookies entrada e bloqueio 
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    //se o url for nulo ou vazio irá para pagina principal
+                    if(string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+            //caso user seja nulo(nao existe) 
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+            return View(loginVM);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Locals/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Locals.ViewModels
+{
+    public class LoginViewModel
+    {
+
+
+        [Required(ErrorMessage ="Necessário informar o nome")]
+        [Display(Name ="Usuário")]
+
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Necessário informar a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
